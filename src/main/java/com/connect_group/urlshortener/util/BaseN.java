79d48bbe5a532421505390c64b4d8fdb506b0b34
Adam Perry <adam@connect-group.com>
2014-12-22 02:14:53 +0000

    Added BaseN utility to convert long's into encoded strings, and vice-versa. Ensured binarySearch used where possible to improve performance.

---
+++ src/main/java/com/connect_group/urlshortener/util/BaseN.java
@@ -0,0 +1,72 @@
+package com.connect_group.urlshortener.util;
+
+/**
+ * Encode and Decode a long value into a String based on an alphabet.
+ */
+public class BaseN {
+    private Alphabet alphabet;
+    private final int radix;
+    
+    public BaseN(String alphabet) {
+        if(StringHelper.isEmpty(alphabet) || alphabet.length()==1) {
+            throw new IllegalArgumentException("alphabet must contain at least 2 entries! ["+alphabet+"]");
+        }
+
+        if(isBinarySearchableAlphabet(alphabet)) {
+            this.alphabet = new OrderedAlphabet(alphabet);
+        } else {
+            this.alphabet = new UnorderedAlphabet(alphabet);
+        }
+        this.radix = this.alphabet.length();
+    }
+
+    private boolean isBinarySearchableAlphabet(String alphabet) {
+        boolean orderedAlphabet=true;
+        char c1=alphabet.charAt(0);
+        for(int i=1; i<alphabet.length(); i++) {
+            char c2 = alphabet.charAt(i);
+            if(c2<c1) {
+                orderedAlphabet=false;
+                break;
+            }
+        }
+        return orderedAlphabet;
+    }
+
+    public String encode(long number) {
+        if(number<0) {
+            throw new IllegalArgumentException("Cannot encode negative number (" + number + ")");
+        }
+        
+        int capacity = (int)(number/radix)+1;
+        int index=capacity-1;
+        char[] encoded = new char[capacity];
+
+        do {
+            long remainder = number % radix;
+            encoded[index--]=alphabet.charAt((int)remainder);
+            number = number / radix;
+        } while(number>0);
+        
+        return new String(encoded);
+    }
+    
+    public long decode(String encoded) {
+        if(StringHelper.isEmpty(encoded)) {
+            throw new IllegalArgumentException("Cannot decode empty or null string");
+        }
+        long result=0;
+        for(int index=0; index<encoded.length(); index++) {
+            result = (result*radix) + indexOf(encoded.charAt(index));
+        }
+        return result;
+    }
+    
+    public boolean isOrderedAlphabet() {
+        return alphabet instanceof OrderedAlphabet;
+    }
+
+    private long indexOf(char c) {
+        return alphabet.indexOf(c);
+    }
+}
---
+++ src/test/java/com/connect_group/urlshortener/util/BaseNTest.java
@@ -0,0 +1,107 @@
+package com.connect_group.urlshortener.util;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class BaseNTest {
+
+    private static final String LARGE_ORDERED_ALPHABET="-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~";
+    private static final String LARGE_UNORDERED_ALPHABET="0123456789-.ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~";
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldThrowException_WhenSuppliedEmptyAlphabet() {
+        new BaseN(null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldThrowException_WhenAlphabetHasOneEntry() {
+        new BaseN("0");
+    }
+
+    @Test
+    public void shouldEncodeZeroUsingFirstAlphabetEntry() {
+        assertThat(new BaseN("01").encode(0L), equalTo("0"));
+    }
+
+    @Test
+    public void shouldEncodeOneUsingSecondAlphabetEntry() {
+        assertThat(new BaseN("01").encode(1L), equalTo("1"));
+    }
+    
+    @Test
+    public void shouldEncodeTwoDigits_WhenValueIsGreaterThanOrEqualToRadix() {
+        assertThat(new BaseN("01").encode(2L), equalTo("10"));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldThrowException_WhenEncodingNegativeNumber() {
+        new BaseN("01").encode(-1L);
+    }
+    
+    @Test
+    public void shouldDecodeFirstAlphabetEntryAsZero_WhenZero() {
+        assertThat(new BaseN("01").decode("0000"), equalTo(0L));
+    }
+
+    @Test
+    public void shouldDecodeSecondAlphabetEntryAsOne_WhenOne() {
+        assertThat(new BaseN("01").decode("1"), equalTo(1L));
+    }
+    
+    @Test
+    public void shouldDecodeMultiCharacterString_WhenTwoCharacters() {
+        assertThat(new BaseN("01").decode("10"), equalTo(2L));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldThrowException_WhenEncodedStringIsEmpty() {
+        new BaseN("01").decode("");
+    }
+    
+    @Test
+    public void shouldIdentifyOrderedAlphabet_WhenAlphabetInAsciiOrder() {
+        assertThat(new BaseN(LARGE_ORDERED_ALPHABET).isOrderedAlphabet(), equalTo(true));
+    }
+
+    @Test
+    public void shouldNotIdentifyOrderedAlphabet_WhenAlphabetNotInAsciiOrder() {
+        assertThat(new BaseN(LARGE_UNORDERED_ALPHABET).isOrderedAlphabet(), equalTo(false));
+    }
+    @Test
+    public void shouldDecodeFaster_WhenOrderedAlphabet() {
+        
+        BaseN fast = new BaseN(LARGE_ORDERED_ALPHABET);
+        BaseN slow = new BaseN(LARGE_UNORDERED_ALPHABET);
+        
+        long start = System.nanoTime();
+        for(int i=0; i<2000000; i++) {
+            fast.decode("bcr89xyAinioqwhfiuqwVYVXUTQIUWZBZ");
+        }
+        long end = System.nanoTime();
+        long fastDuration = end-start;
+
+        start = System.nanoTime();
+        for(int i=0; i<2000000; i++) {
+            slow.decode("bcr89xyAinioqwhfiuqwVYVXUTQIUWZBZ");
+        }
+        end = System.nanoTime();
+        long slowDuration = end-start;
+
+
+        if(slowDuration>fastDuration) {
+            long diff = slowDuration-fastDuration;
+            System.out.println("Ordered Alphabet was "+diff +"ns ("+diff/1000000+"ms, "+ (diff*100)/(slowDuration) +"%) faster");
+        } else {
+            long diff = fastDuration-slowDuration;
+            System.out.println("Ordered Alphabet was "+diff +"ns ("+diff/1000000+"ms) slower");
+        }
+        
+        // want to beat 350ms
+        
+        //2903000L, 12453000L, 25132000L
+        assertThat(slowDuration-fastDuration, greaterThan(0L));
+    }
+}
